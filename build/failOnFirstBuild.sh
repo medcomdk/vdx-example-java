090e08122c4458216b4477bf0027e1c245223391
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-09-01 10:53:05 +0200

    Clean up build.

--- build/failOnFirstBuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "${GITHUB_REPOSITORY}"
-echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/vdx-example-java" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/vdx-example-java" ]; then
-  echo "Please run setup.sh REPOSITORY_NAME"
-  exit 1
-fi
